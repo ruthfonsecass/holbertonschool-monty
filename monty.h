d617b400139296c08dc2c8aff9bf60c2a1af89e1
benjaminparentt <p.benjamin620@icloud.com>
2024-01-04 08:11:47 -0800

    change fonction name err

--- monty.h
+++ monty.h
@@ -1,13 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#define _GNU_SOURCE
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -39,46 +37,35 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern stack_t *head;
-typedef void (*op_func)(stack_t **, unsigned int);
-
-/*file operations*/
-void open_file(char *file_name);
-int parse_line(char *buffer, int line_number, int format);
-void read_file(FILE *);
-int len_chars(FILE *);
-void find_func(char *, char *, int, int);
-
-/*Stack operations*/
-stack_t *create_node(int n);
-void free_nodes(void);
-void print_stack(stack_t **, unsigned int);
-void add_to_stack(stack_t **, unsigned int);
-void add_to_queue(stack_t **, unsigned int);
-
-void call_fun(op_func, char *, char *, int, int);
-
-void print_top(stack_t **, unsigned int);
-void pop_top(stack_t **, unsigned int);
-void nop(stack_t **, unsigned int);
-void swap_nodes(stack_t **, unsigned int);
-
-/*Math operations with nodes*/
-void add_nodes(stack_t **, unsigned int);
-void sub_nodes(stack_t **, unsigned int);
-void div_nodes(stack_t **, unsigned int);
-void mul_nodes(stack_t **, unsigned int);
-void mod_nodes(stack_t **, unsigned int);
-
-/*String operations*/
-void print_char(stack_t **, unsigned int);
-void print_str(stack_t **, unsigned int);
-void rotl(stack_t **, unsigned int);
-
-/*Error hanlding*/
+/**
+ * Function prototypes
+ */
 void err(int error_code, ...);
 void more_err(int error_code, ...);
 void string_err(int error_code, ...);
-void rotr(stack_t **, unsigned int);
-extern stack_t *head;
-#endif
+
+stack_t *create_node(int n);
+void free_nodes(void);
+
+void open_file(char *file_name);
+void read_file(FILE *fd);
+int parse_line(char *buffer, int line_number, int format);
+void find_func(char *opcode, char *value, int ln, int format);
+void call_func(instruction_t func, char *op, char *val, int ln, int format);
+
+void push_to_stack(stack_t **new_node, unsigned int line_number);
+void print_stack_nodes(stack_t **stack, unsigned int line_number);
+void pop_stack_top(stack_t **stack, unsigned int line_number);
+void print_stack_top(stack_t **stack, unsigned int line_number);
+
+void add_to_queue(stack_t **new_node, unsigned int ln);
+void multiply_stack_nodes(stack_t **stack, unsigned int line_number);
+void modulo_stack_nodes(stack_t **stack, unsigned int line_number);
+
+void print_ascii_char(stack_t **stack, unsigned int line_number);
+void print_ascii_str(stack_t **stack, unsigned int line_number);
+void rotate_left(stack_t **stack, unsigned int line_number);
+void rotate_right(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
+
